9f93fcba483c64aebbcabbfaef7907ddec2aab76
Auk1003 <Aukown1003@users.noreply.github.com>
2023-03-14 20:17:03 +0900

    Initial Commit

---
+++ SwiftUI-Timer/SwiftUI_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TimerApp.swift
+//  SwiftUI-Timer
+//
+//  Created by 桑野友輔 on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
